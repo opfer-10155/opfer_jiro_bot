9fd40af1222708a1dcf82d199318f30a5bff732b
opfer <OPF-10155@outlook.jp>
2018-07-14 03:06:49 +0900

    初期ソースコード追加

---
+++ main.rb
@@ -0,0 +1,43 @@
+require "twitter"
+require "dotenv"
+
+Dotenv.load
+
+random = Random.new
+
+client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV["MY_CONSUMER_KEY"]
+    config.consumer_secret     = ENV["MY_CONSUMER_SECRET"]
+    config.access_token        = ENV["MY_ACCESS_TOKEN"]
+    config.access_token_secret = ENV["MY_ACCESS_TOKEN_SECRET"]
+  end
+
+client_streaming = Twitter::Streaming::Client.new do |config|
+    config.consumer_key = ENV["MY_CONSUMER_KEY"]
+    config.consumer_secret = ENV["MY_CONSUMER_SECRET"]
+    config.access_token = ENV["MY_ACCESS_TOKEN"]
+    config.access_token_secret =  ENV["MY_ACCESS_TOKEN_SECRET"]
+  end
+
+  client_streaming.user do |tweet|
+   
+    if tweet.is_a?(Twitter::Tweet)
+      puts(tweet.user.name)
+      puts("@#{tweet.user.screen_name}")
+      puts(tweet.text)
+      puts("-----")
+      z = random.rand(1..114514)
+    
+     if /#オプファーは二郎を奢れ/ =~ tweet.text
+
+      if z == 810
+        client.update("@#{tweet.user.screen_name}\n奢ります", options = {:in_reply_to_status_id => tweet.id})
+
+      else  client.update("@#{tweet.user.screen_name}\n奢りません", options = {:in_reply_to_status_id => tweet.id})
+
+      end
+
+     end
+     
+    end
+end
---
+++ randomtest.rb
@@ -0,0 +1,27 @@
+random = Random.new
+a = random.rand(1..810)
+puts("#{a}-")
+puts("#{a}-")
+puts("#{a}-")
+puts("#{a}-")
+puts("#{a}-")
+
+puts("#{random.rand(1..114514)}")
+puts("#{random.rand(1..114514)}")
+puts("#{random.rand(1..114514)}")
+puts("#{random.rand(1..114514)}")
+puts("#{random.rand(1..114514)}")
+
+puts("#{random.rand(1..114514)==810}")
+
+
+puts("#{random.rand(1..2)==2}")
+puts("#{random.rand(1..2)==2}")
+puts("#{random.rand(1..2)==2}")
+puts("#{random.rand(1..2)==2}")
+puts("#{random.rand(1..2)==2}")
+puts("#{random.rand(1..2)==2}")
+puts("#{random.rand(1..2)==2}")
+puts("#{random.rand(1..2)==2}")
+puts("#{random.rand(1..2)==2}")
+puts("#{random.rand(1..2)==2}")
