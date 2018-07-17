681e3c443d4c03f789eaa48dfcef82ba9ac7d384
opfer <OPF-10155@outlook.jp>
2018-07-17 23:25:10 +0900

    確率を1/1919にした

--- main.rb
+++ main.rb
@@ -29,7 +29,9 @@ client_streaming = Twitter::Streaming::Client.new do |config|
       
     
      if /#オプファーは二郎を奢れ/ =~ tweet.text
-      z = random.rand(1..114514)
+      z = random.rand(1..1919)
+      #timesail = random.rand(1..810)
+    
       if z == 810
         client.update("@#{tweet.user.screen_name}\n奢ります", options = {:in_reply_to_status_id => tweet.id})
 
