ce59011fa245a9e5800d9be56bac066be7c843dd
opfer <OPF-10155@outlook.jp>
2018-10-07 01:14:39 +0900

    完成

--- main.rb
+++ main.rb
@@ -12,22 +12,28 @@ client = Twitter::REST::Client.new do |config|
     config.access_token_secret =  "zV2T5YY1E20Bf2zrKlTmfARBeXiRcPhSKCzmfbZ0pw4M0"
 end
 
-    max_id = client.home_timeline.first.id
+    since_id = client.home_timeline.first.id
+    #max_id = client.home_timeline.first.id
     525600.times do
-      client.home_timeline(max_id: max_id,count: 20).each do |tweet|
-
+      define_since_id = true
+      client.home_timeline(since_id: since_id, count: 100).each do |tweet|
+        if !tweet.retweeted?
         puts(tweet.user.name)
         puts("@#{tweet.user.screen_name}")
         puts(tweet.text)
         puts("-----")
-        max_id = tweet.id unless tweet.retweeted?
+        if define_since_id
+          since_id = tweet.id
+          define_since_id = false
+        end
+        #max_id = tweet.id unless tweet.retweeted?
 
         if /#オプファーは二郎を奢れ/ =~ tweet.text
           ogoru = random.rand(1..260)
-      
+          
           if ogoru == 19
             client.update("@#{tweet.user.screen_name}\n奢ります", options = {:in_reply_to_status_id => tweet.id})
-  
+          
           else  
 
             File.open("variety.txt", "r") do |bot|
@@ -36,7 +42,8 @@ end
               client.update("@#{tweet.user.screen_name}\n#{hazure}", options = {:in_reply_to_status_id => tweet.id})
             end
           end
+          end
         end
       end
-      sleep 60
+      sleep 64
     end
