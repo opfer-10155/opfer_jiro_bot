c67f12cb139a41d6e1a7d9ef0603b4e68ebd9d3b
opfer <OPF-10155@outlook.jp>
2019-03-25 12:22:33 +0900

    軽量化

--- main.rb
+++ main.rb
@@ -12,38 +12,30 @@ client = Twitter::REST::Client.new do |config|
     config.access_token_secret =  "zV2T5YY1E20Bf2zrKlTmfARBeXiRcPhSKCzmfbZ0pw4M0"
 end
 
-    since_id = client.home_timeline.first.id
-    #max_id = client.home_timeline.first.id
-    525600.times do
-      define_since_id = true
-      client.home_timeline(since_id: since_id, count: 1000).each do |tweet|
-        if define_since_id
-          since_id = tweet.id
-          define_since_id = false
-        end
-        #max_id = tweet.id unless tweet.retweeted?
-        if !tweet.retweeted?
-          if /#オプファーは二郎を奢れ/ =~ tweet.text
-            ogoru = random.rand(1..200)
-            puts(ogoru)
-            puts(tweet.user.name)
-            puts("@#{tweet.user.screen_name}")
-            puts(tweet.text)
-            puts("-----")
-            if ogoru == 19
-              client.update("@#{tweet.user.screen_name}\n奢ります", options = {:in_reply_to_status_id => tweet.id})
-          
-            else  
+since_id = client.home_timeline.first.id
+525600.times do
+  define_since_id = true
+  client.home_timeline(since_id: since_id, count: 1000).each do |tweet|
+    if define_since_id
+      since_id = tweet.id
+      define_since_id = false
+    end
+    if !tweet.retweeted?
+      if /#オプファーは二郎を奢れ/ =~ tweet.text
+        ogoru = random.rand(1..200)
+        if ogoru == 19
+          client.update("@#{tweet.user.screen_name}\n奢ります", options = {:in_reply_to_status_id => tweet.id})
+      
+        else  
 
-              File.open("variety.txt", "r") do |bot|
-                @bots = bot.read.split("\n")
-                hazure = @bots.sample
-                client.update("@#{tweet.user.screen_name}\n#{hazure}", options = {:in_reply_to_status_id => tweet.id})
-                puts(hazure)
-              end
-            end
+          File.open("variety.txt", "r") do |bot|
+            @bots = bot.read.split("\n")
+            hazure = @bots.sample
+            client.update("@#{tweet.user.screen_name}\n#{hazure}", options = {:in_reply_to_status_id => tweet.id})
           end
         end
       end
-      sleep 64
     end
+  end
+  sleep 64
+end
